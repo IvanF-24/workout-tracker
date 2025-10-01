df8e3706ff3fc320e4c03b92ab9cb9dce7e40538
IvanF-24 <ivanflorez134@gmail.com>
2025-10-01 12:22:10 -0500

    scaffold

---
+++ src/controllers/exercises.controller.js
@@ -0,0 +1,13 @@
+// src/controllers/exercises.controller.js
+
+// Estado en memoria (simulación)
+let exercises = [
+  {
+    id: "101",
+    nombre: "Press de banca",
+    descripcion: "Ejercicio de fuerza para trabajar el pecho y tríceps.",
+    categoria: "fuerza",
+    grupo_muscular: "pecho"
+  }
+];
+
