fa673661eed665481c25dc243d1047d178a28d3c
IvanF-24 <ivanflorez134@gmail.com>
2025-10-01 14:51:51 -0500

    scaffold

---
+++ src/controllers/workoutSchedule.controller.js
@@ -0,0 +1,13 @@
+// src/controllers/workoutSchedule.controller.js
+
+// Estado en memoria (simulación)
+let workoutSchedules = [
+  {
+    id: "4001",
+    plan_id: 2001,
+    usuario_id: 1,
+    fecha_hora: "2025-09-21T18:00:00Z",
+    estado: "pendiente"
+  }
+];
+
