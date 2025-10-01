8766f3c5ffac65981d1f3d11cfc343e5ea0e2c15
IvanF-24 <ivanflorez134@gmail.com>
2025-10-01 15:12:02 -0500

    WorkoutReports/scaffold

---
+++ src/controllers/workoutReports.controller.js
@@ -0,0 +1,17 @@
+// Estado en memoria (simulación)
+let workoutReports = [
+  {
+    id: 6001,
+    usuario_id: 1,
+    fecha_inicio: "2025-08-01",
+    fecha_fin: "2025-08-31",
+    total_entrenamientos: 12,
+    ejercicios_mas_frecuentes: ["Sentadillas", "Press de banca", "Dominadas"],
+    mejora_por_grupo_muscular: {
+      pecho: "+10 kg en press banca",
+      piernas: "+15 kg en sentadillas",
+      espalda: "Mayor número de repeticiones en dominadas"
+    }
+  }
+];
+
